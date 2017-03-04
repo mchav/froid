21a6c3c0d008505cc958dedf0dc5c0b94ecabc1a
ChavXO <mchavinda@colgate.edu>
2017-03-04 02:10:41 -0500

    Automatically put rt.jar in class path

--- compile.ps1
+++ compile.ps1
@@ -1,5 +1,11 @@
-(mkdir build\froid\app) 2> $null
+(mkdir build\froid\app) 2>&1> $null
 $jars = Get-ChildItem -File -Attributes !ReadOnly -path ".\lib" | % { $_.FullName }
 $cpJars = [string]::Join(";", $jars)
-javac -cp """$cpJars""" -bootclasspath "C:\Program Files\Java\jre1.8.0_111\lib\rt.jar" -source 1.7 -target 1.7 -d .\build .\src\java\*
-java -Xmx6072m -Xss10M -XX:MaxJavaStackTraceDepth=-1 -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none -cp """$cpJars""" frege.compiler.Main -target 1.7 -d .\build .\src\frege\froid\text\Editable.fr
+
+($runtime = java -verbose) 2>&1> $null
+
+$s = ($runtime -split '\n')[0]
+$rtjar = $s.Substring(8, $s.length - 9)
+
+javac -cp """$cpJars""" -bootclasspath """$rtjar""" -source 1.7 -target 1.7 -d .\build .\src\java\*
+java -Xmx6072m -Xss10M -XX:MaxJavaStackTraceDepth=-1 -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none -cp """$cpJars""" frege.compiler.Main -target 1.7 -d .\build .\src\frege\froid\
